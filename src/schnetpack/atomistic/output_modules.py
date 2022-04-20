4db1edf4d49c0c750d196ffbf76ed968483f994e
RubenStaub <RubenStaub@users.noreply.github.com>
2022-04-20 18:30:45 +0900

    Removed doc duplicate

--- src/schnetpack/atomistic/output_modules.py
+++ src/schnetpack/atomistic/output_modules.py
@@ -193,44 +193,7 @@ class AtomwiseZBL(Atomwise):
     """
     Predicts atom-wise energy contributions (with ZBL correction) and accumulates global prediction.
 
-    Args:
-        n_in (int): input dimension of representation
-        n_out (int): output dimension of target property (default: 1)
-        aggregation_mode (str): one of {sum, avg} (default: sum)
-        n_layers (int): number of nn in output network (default: 2)
-        n_neurons (list of int or None): number of neurons in each layer of the output
-            network. If `None`, divide neurons by 2 in each layer. (default: None)
-        activation (function): activation function for hidden nn
-            (default: spk.nn.activations.shifted_softplus)
-        property (str): name of the output property (default: "y")
-        contributions (str or None): Name of property contributions in return dict.
-            No contributions returned if None. (default: None)
-        derivative (str or None): Name of property derivative. No derivative
-            returned if None. (default: None)
-        negative_dr (bool): Multiply the derivative with -1 if True. (default: False)
-        stress (str or None): Name of stress property. Compute the derivative with
-            respect to the cell parameters if not None. (default: None)
-        create_graph (bool): If False, the graph used to compute the grad will be
-            freed. Note that in nearly all cases setting this option to True is not
-            needed and often can be worked around in a much more efficient way.
-            Defaults to the value of create_graph. (default: False)
-        mean (torch.Tensor or None): mean of property
-        stddev (torch.Tensor or None): standard deviation of property (default: None)
-        atomref (torch.Tensor or None): reference single-atom properties. Expects
-            an (max_z + 1) x 1 array where atomref[Z] corresponds to the reference
-            property of element Z. The value of atomref[0] must be zero, as this
-            corresponds to the reference property for for "mask" atoms. (default: None)
-        outnet (callable): Network used for atomistic outputs. Takes schnetpack input
-            dictionary as input. Output is not normalized. If set to None,
-            a pyramidal network is generated automatically. (default: None)
-
-    Returns:
-        tuple: prediction for property
-
-        If contributions is not None additionally returns atom-wise contributions.
-
-        If derivative is not None additionally returns derivative w.r.t. atom positions.
-
+    See Atomwise module documentation for more details.
     """
 
     def forward(self, inputs):
