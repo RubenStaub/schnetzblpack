d97930823071c4ff2e946537e610c8434202448b
RubenStaub <RubenStaub@users.noreply.github.com>
2022-04-20 15:58:28 +0900

    Added ZBL support

--- src/schnetpack/atomistic/output_modules.py
+++ src/schnetpack/atomistic/output_modules.py
@@ -5,6 +5,7 @@ from torch.autograd import grad
 
 import schnetpack
 from schnetpack import nn as L, Properties
+from .zbl import ZBLRepulsionEnergy
 
 __all__ = [
     "Atomwise",
@@ -188,6 +189,110 @@ class Atomwise(nn.Module):
 
         return result
 
+class AtomwiseZBL(Atomwise):
+    """
+    Predicts atom-wise energy contributions (with ZBL correction) and accumulates global prediction.
+
+    Args:
+        n_in (int): input dimension of representation
+        n_out (int): output dimension of target property (default: 1)
+        aggregation_mode (str): one of {sum, avg} (default: sum)
+        n_layers (int): number of nn in output network (default: 2)
+        n_neurons (list of int or None): number of neurons in each layer of the output
+            network. If `None`, divide neurons by 2 in each layer. (default: None)
+        activation (function): activation function for hidden nn
+            (default: spk.nn.activations.shifted_softplus)
+        property (str): name of the output property (default: "y")
+        contributions (str or None): Name of property contributions in return dict.
+            No contributions returned if None. (default: None)
+        derivative (str or None): Name of property derivative. No derivative
+            returned if None. (default: None)
+        negative_dr (bool): Multiply the derivative with -1 if True. (default: False)
+        stress (str or None): Name of stress property. Compute the derivative with
+            respect to the cell parameters if not None. (default: None)
+        create_graph (bool): If False, the graph used to compute the grad will be
+            freed. Note that in nearly all cases setting this option to True is not
+            needed and often can be worked around in a much more efficient way.
+            Defaults to the value of create_graph. (default: False)
+        mean (torch.Tensor or None): mean of property
+        stddev (torch.Tensor or None): standard deviation of property (default: None)
+        atomref (torch.Tensor or None): reference single-atom properties. Expects
+            an (max_z + 1) x 1 array where atomref[Z] corresponds to the reference
+            property of element Z. The value of atomref[0] must be zero, as this
+            corresponds to the reference property for for "mask" atoms. (default: None)
+        outnet (callable): Network used for atomistic outputs. Takes schnetpack input
+            dictionary as input. Output is not normalized. If set to None,
+            a pyramidal network is generated automatically. (default: None)
+
+    Returns:
+        tuple: prediction for property
+
+        If contributions is not None additionally returns atom-wise contributions.
+
+        If derivative is not None additionally returns derivative w.r.t. atom positions.
+
+    """
+
+    def forward(self, inputs):
+        r"""
+        predicts atomwise property
+        """
+        atomic_numbers = inputs[Properties.Z]
+        atom_mask = inputs[Properties.atom_mask]
+
+        # run prediction
+        yi = self.out_net(inputs)
+        yi = self.standardize(yi)
+
+        if self.atomref is not None:
+            y0 = self.atomref(atomic_numbers)
+            yi = yi + y0
+        
+        # add ZBL energy repulsion
+        zbl_corr = ZBLRepulsionEnergy(inputs)
+        yi += zbl_corr
+
+        y = self.atom_pool(yi, atom_mask)
+
+        # collect results
+        result = {self.property: y, 'zbl': zbl_corr}
+
+        if self.contributions is not None:
+            result[self.contributions] = yi
+
+        create_graph = True if self.training else self.create_graph
+
+        if self.derivative is not None:
+            sign = -1.0 if self.negative_dr else 1.0
+            dy = grad(
+                result[self.property],
+                inputs[Properties.R],
+                grad_outputs=torch.ones_like(result[self.property]),
+                create_graph=create_graph,
+                retain_graph=True,
+            )[0]
+            result[self.derivative] = sign * dy
+
+        if self.stress is not None:
+            cell = inputs[Properties.cell]
+            # Compute derivative with respect to cell displacements
+            stress = grad(
+                result[self.property],
+                inputs["displacement"],
+                grad_outputs=torch.ones_like(result[self.property]),
+                create_graph=create_graph,
+                retain_graph=True,
+            )[0]
+            # Compute cell volume
+            volume = torch.sum(
+                cell[:, 0, :] * torch.cross(cell[:, 1, :], cell[:, 2, :], dim=1),
+                dim=1,
+                keepdim=True,
+            )[..., None]
+            # Finalize stress tensor
+            result[self.stress] = stress / volume
+
+        return result
 
 class DipoleMoment(Atomwise):
     """
