3758c9433cc2421e4a0793ac44bff929f69df748
RubenStaub <RubenStaub@users.noreply.github.com>
2022-04-21 07:56:42 +0900

    Add softplus inverse

--- src/schnetpack/nn/activations.py
+++ src/schnetpack/nn/activations.py
@@ -1,6 +1,8 @@
 import numpy as np
 from torch.nn import functional
 
+__all__ = ["shifted_softplus", "softplus_inverse"]
+
 
 def shifted_softplus(x):
     r"""Compute shifted soft-plus activation function.
@@ -16,3 +18,12 @@ def shifted_softplus(x):
 
     """
     return functional.softplus(x) - np.log(2.0)
+
+
+def softplus_inverse(x):
+    """Inverse softplus transformation. This is useful for initialization of parameters
+    that are constrained to be positive.
+    """
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x)
+    return x + torch.log(-torch.expm1(-x))
